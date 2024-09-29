c6b793cbf86beee65926b68e24247cf8d7f377b2
Rodrigo Barbosa <59710101+rodrigo210686@users.noreply.github.com>
2024-09-29 16:51:10 -0300

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,44 @@
+
+# Security Group for ALB
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# Create ALB
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id, aws_subnet.public_subnet2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "app-alb"
+  }
+}
+
+
+# Output ALB DNS Name
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
